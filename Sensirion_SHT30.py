8d75eabbde6a0007f9d6a75bb95fec1ef30b2007
Janique <janique.arnold@lis.ch>
2021-03-30 13:33:08 +0200

    bugfix

--- Sensirion_SHT30.py
+++ Sensirion_SHT30.py
@@ -15,5 +15,5 @@ class Sensirion_SHT30():
         sht30_bus.write_i2c_block_data(0x44, 0x2C, [0x06])
         time.sleep(0.5)
         data = sht30_bus.read_i2c_block_data(0x44, 0x00, 6)
-        self.temp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
+        self.temperature = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
         self.humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
