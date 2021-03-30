8e564dc031b9d274b580dba19b3724c4b64465d0
Janique <janique.arnold@lis.ch>
2021-03-30 14:17:26 +0200

    bugfix

--- example_sgp40_and_sht30_voc_index.py
+++ example_sgp40_and_sht30_voc_index.py
@@ -14,5 +14,5 @@ sgp40.begin(10)
 while True:
     sht30.sht30_read()
     sgp40.set_envparams(sht30.humidity, sht30.temperature)
-    print('Voc index : %d'%(sgp40.get_voc_index()))
+    print('Voc index:  %d ppm Temperature: %8.2f°C humidity: %8.2frh' % (sgp40.get_voc_index(), sht30.temperature, sht30.humidity))
     time.sleep(1)
